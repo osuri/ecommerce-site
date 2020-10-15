94ec24a4ec84a23c6dbc38e72ac78ccfc161abef
Lokanadham Osuri <losuri@GLIC.COM>
2020-10-15 10:55:10 +0530

    updated the hats menu with rouitng

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
 
 function App() {
+  const HatsPage = () => {
+    return (
+      <div>
+        <h1>Hats page</h1>
+      </div>
+    );
+  };
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
