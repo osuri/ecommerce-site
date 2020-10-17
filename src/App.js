152dbe6643f23d14001f2459df08d0006b81a593
Lokanadham Osuri <losuri@GLIC.COM>
2020-10-17 19:09:59 +0530

    updated with shop page

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shoppage.component.jsx";
 
 function App() {
-  const HatsPage = () => {
-    return (
-      <div>
-        <h1>Hats page</h1>
-      </div>
-    );
-  };
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
