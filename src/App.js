c6c57543dd5c8cf64f55163214b27ecdafe64698
Lokanadham Osuri <losuri@GLIC.COM>
2020-10-17 19:49:12 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shoppage.component.jsx";
+import Header from "./components/header/header.component.jsx";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
